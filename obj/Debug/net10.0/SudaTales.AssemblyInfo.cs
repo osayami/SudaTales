818651b63f447f3e48497aa9c77dd6a46b63b449
osayami <24865842+osayami@users.noreply.github.com>
2025-12-25 23:20:05 +0530

    fixed gitignore and base url

--- obj/Debug/net10.0/SudaTales.AssemblyInfo.cs
+++ obj/Debug/net10.0/SudaTales.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SudaTales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e666e5bcaebd05839b73f331aa03aedfce2e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8892985219b3baa79b8fbc5140ac79bfdda05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SudaTales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SudaTales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
